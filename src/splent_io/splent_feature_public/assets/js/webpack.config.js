32b05df172496b21fbc665793c5de35cf7ae87b2
drorganvidez <davromorg@alum.us.es>
2026-03-26 01:01:45 +0100

    fix: webpack bundle filename to match template references, bump v1.2.2

--- src/splent_io/splent_feature_public/assets/js/webpack.config.js
+++ src/splent_io/splent_feature_public/assets/js/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: path.resolve(__dirname, './scripts.js'),
   output: {
-    filename: 'splent_feature_public.bundle.js',
+    filename: 'public.bundle.js',
     path: path.resolve(__dirname, '../dist'),
   },
   resolve: {

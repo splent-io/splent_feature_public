33a890a57efbfa8e9c5f0022775dd0efa5564711
drorganvidez <davromorg@alum.us.es>
2026-03-31 00:52:16 +0000

    chore: release splent_io/splent_feature_public to v1.5.3

--- src/splent_io/splent_feature_public/hooks.py
+++ src/splent_io/splent_feature_public/hooks.py
@@ -3,7 +3,11 @@ from flask import url_for
 
 
 def public_scripts():
-    return '<script src="' + url_for('public.assets', subfolder='dist', filename='public.bundle.js') + '"></script>'
+    return (
+        '<script src="'
+        + url_for("public.assets", subfolder="dist", filename="public.bundle.js")
+        + '"></script>'
+    )
 
 
 register_template_hook("layout.scripts", public_scripts)
